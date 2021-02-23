55df17e6285a330df467fa518d5c5ac08b7c0648
Brandon Service <brservice@comcast.net>
2021-02-23 14:46:45 -0500

    Platforms and character test added

---
+++ Assets/Scripts/PlayerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = (new Vector2(0, 30));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-6, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -6));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(6, 0));
+        }
+
+        //GetComponent<Rigidbody2D>().
+    }
+}
