298bf726adc134c6445e25ecd186eba733b1b187
Brandon Service <brservice@comcast.net>
2021-02-26 17:19:41 -0500

    Adding enemies who can move and be attacked

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D triggerBox;
+    private Vector2 velocity;
+
+    public float loc1;
+    public float loc2;
+    public float speed;
+
+    private bool left;
+    private bool right;
+    private bool attack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Random.Range(0, 1) == 1)
+        { 
+            right = true;
+            left = false;
+        }
+        else
+        {
+            left = true;
+            right = false;
+        }
+        attack = false;
+        velocity = new Vector2();
+        rigidBody = GetComponent<Rigidbody2D>();
+        triggerBox = GetComponentInChildren<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        MoveBackAndForth();
+
+        rigidBody.position += velocity * Time.deltaTime;
+        velocity = Vector2.zero;
+    }
+
+    public void MoveBackAndForth()
+    {
+        if (right)
+        {
+            if (rigidBody.position.x >= loc1)
+            {
+                right = !right;
+                left = !left;
+            }
+            velocity = new Vector2(1, 0) * speed;
+        }
+        if (left)
+        {
+            if (rigidBody.position.x <= loc2)
+            {
+                right = !right;
+                left = !left;
+            }
+            velocity = new Vector2(-1, 0) * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            attack = true;
+            Debug.Log("attack now");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            attack = false;
+            Debug.Log("can't attack");
+        }
+    }
+}
--- Assets/Scripts/PlayerTest.cs
+++ Assets/Scripts/PlayerTest.cs
@@ -4,32 +4,40 @@ using UnityEngine;
 
 public class PlayerTest : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocity = new Vector2();
+        acceleration = new Vector2();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //rigidBody.AddForce(new Vector2(0, -1));
         if (Input.GetKeyDown(KeyCode.W))
         {
-            GetComponent<Rigidbody2D>().velocity = (new Vector2(0, 30));
+            //rigidBody.AddForce(new Vector2(0, 30));
+            //velocity = new Vector2(0, 300);
+            rigidBody.position += (new Vector2(0, 6));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(-6, 0));
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -6));
+            //rigidBody.AddForce(new Vector2(-6, 0) );
+            velocity = new Vector2(-6, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(6, 0));
+            //rigidBody.AddForce(new Vector2(6, 0) * Time.deltaTime);
+            velocity = new Vector2(6, 0);
         }
 
-        //GetComponent<Rigidbody2D>().
+        rigidBody.position += velocity * Time.deltaTime;
+        velocity = Vector2.zero;
     }
 }
