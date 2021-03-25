2aa80497e77413828b57a103c4737ee55d75cf68
Ryan Rieger <rmr6894@rit.edu>
2021-03-25 00:46:32 -0400

    Added coins
    Each coin collected adds 50 to the player's score.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private GameObject sceneController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneController = GameObject.Find("SceneController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // increase the score when the player touches a coin and destroy the coin
+        if (collision.tag == "Player")
+        {
+            sceneController.GetComponent<SceneManagement>().score += 50;
+            Destroy(this);
+            Destroy(gameObject);
+        }
+    }
+}
