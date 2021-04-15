f9de3c352cb6d604eb6a398bf8b4b5a18394c383
Brandon Service <brservice@comcast.net>
2021-04-14 21:56:45 -0400

    Creating Onboarding

---
+++ Assets/Scripts/Onboarding.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onboarding : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //text.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            text.SetActive(true);
+        }        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            text.SetActive(false);
+        }
+    }
+}
